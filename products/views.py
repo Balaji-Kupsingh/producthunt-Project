2159a1d5b44def891b6ae48f6bf31c9fdc0db11e
balajikupsingh <balajikupsingh7@gmail.com>
2020-08-07 16:31:51 +0530

    ProductHunt Project

---
+++ products/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render , redirect , get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Products
+from django.utils import timezone
+
+def home(request):
+    product=Products.objects
+    return render (request,'products/home.html',{'products':product})
+
+@login_required(login_url = "/accounts/signup")
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image'] :
+            products = Products()
+            products.title = request.POST['title']
+            products.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                    products.url = request.POST['url']
+            else:
+                products.url= 'http://' + request.POST['url']
+                products.icon = request.FILES['icon']
+                products.image = request.FILES['image']
+                products.pub_date = timezone.datetime.now()
+                products.hunter = request.user
+                products.save()
+                return redirect('/products/' + str(products.id))
+        else:
+            return render (request,'products/create.html', {'error':'fill all the options'})
+
+    else:
+        return render (request,'products/create.html' )
+
+def detail(request, product_id ):
+    detailproduct = get_object_or_404( Products , pk=product_id )
+    return render (request,'products/detail.html',{'products':detailproduct})
+
+@login_required(login_url = "/accounts/signup")
+def upvote(request , product_id):
+    if request.method == 'POST':
+        products = get_object_or_404( Products , pk=product_id )
+        products.votes_total += 1
+        products.save()
+        return redirect('/products/' + str(products.id))
